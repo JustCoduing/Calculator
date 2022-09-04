5413201c23fff517485c0045d3c9eb3273a1804f
Khan Hassan <khanhassan1401@gmail.com>
2022-09-04 10:32:48 +0200

    started js by first modifiying 0-9 Str into number

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const numbers = document.querySelectorAll(".options button.number")
+numbers.forEach(number=>{
+    number.addEventListener("click",showNumber)
+})
 
+function showNumber()
+{
+   let number = Number(this.textContent)
+   console.log(number)
 
-
-
-
-
+}
 
