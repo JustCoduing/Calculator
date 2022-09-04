f549636a2d865dbafd5e0c9e3aa01308c68c6b36
Khan Hassan <khanhassan1401@gmail.com>
2022-09-04 11:20:53 +0200

    Clear Display By Clicking Clear Button

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const numbers = document.querySelectorAll(".options button.number")
 const typeDisplay = document.querySelector(".type")
 const typeDisplayPara = document.querySelector(".type p")
+const result = document.querySelector(".result")
+const resultDisplayPara = document.querySelector(".result p")
+const clear = document.querySelector(".clear")
 
 
 numbers.forEach(number=>{
@@ -28,4 +31,13 @@ function showOper()
 {
     console.log(this.textContent)
     typeDisplayPara.append(this.textContent)
+}
+
+
+clear.addEventListener("click",clearDisplay)
+
+function clearDisplay()
+{
+    typeDisplayPara.textContent=" "
+    resultDisplayPara.textContent=" "
 }
