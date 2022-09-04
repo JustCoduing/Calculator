58c41e7b92e6e4f099e058f4b805848b282588e9
Khan Hassan <khanhassan1401@gmail.com>
2022-09-04 11:00:45 +0200

    Display Number On Calculator

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const numbers = document.querySelectorAll(".options button.number")
+const typeDisplay = document.querySelector(".type")
+
+
 numbers.forEach(number=>{
     number.addEventListener("click",showNumber)
 })
-
+let number 
 function showNumber()
 {
-   let number = Number(this.textContent)
+    let typeDisplayPara = document.createElement("p")
+    typeDisplayPara.classList.add("style-num")
+   number = Number(this.textContent)
    console.log(number)
+   typeDisplayPara.innerText=number
+   typeDisplay.append(typeDisplayPara)
+   
 
 }
 
+
+
+
+const operators = document.querySelectorAll(".options button.operator")
+operators.forEach(operator=>{
+operator.addEventListener("click",showOper)
+})
+
+
+
+function showOper()
+{
+    console.log(this.textContent)
+}
